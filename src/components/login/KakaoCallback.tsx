4b38e2e970d27bf91cb657e72c54ad2f03f61931
jungsunbeen <tjsqls041103@gmail.com>
2025-05-16 14:20:21 +0900

    feat: Kakao 로그인 기능 추가 및 라우팅 업데이트

---
+++ src/components/login/KakaoCallback.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+const KakaoCallback = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const code = queryParams.get('code');
+    console.log('인가 코드:', code);
+
+    if (code) {
+      axios.post('http://localhost:8080/api/auth/kakao', {
+        code: code
+      })
+      .then(response => {
+        console.log('로그인 성공', response.data);
+        localStorage.setItem('token', response.data.token);
+        window.location.href = '/';
+      })
+      .catch(error => {
+        console.error('로그인 실패', error);
+      });
+    }
+  }, [location]);
+
+  return <div>로그인 중입니다...</div>;
+};
+
+export default KakaoCallback;
