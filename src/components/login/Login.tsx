7e55e83d692ebdd7a57a0606f8a5597ae400f2c2
jungsunbeen <tjsqls041103@gmail.com>
2025-05-16 15:04:58 +0900

    Refactor: code structure for improved readability and maintainability

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 const Login = () => {
   const REST_API_KEY = 'b57e43877ad662a5a26d85d3b6ff834e';
   const REDIRECT_URI = 'http://localhost:8080/oauth/kakao/callback';
@@ -8,10 +10,26 @@ const Login = () => {
   };
 
   return (
-    <button type='button' onClick={loginHandler}>
-      로그인 하기
-    </button>
+    <Button type='button' onClick={loginHandler}>
+      카카오 로그인
+    </Button>
   );
 };
 
 export default Login;
+
+const Button = styled.button`
+  background-color: #fee500;
+  width: 50%;
+  height: 45px;
+  color: #000;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #ffd700;
+  }
+`
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,28 @@
+import styled from "styled-components";
 import Login from "../components/login/Login";
 
 
 const LoginPage = () => {
   return (
-    <>
+    <Wrapper>
+    <Logo src="/images/icon.png" alt="logo" />
     <Login />
-    </>
+    </Wrapper>
   );
 }
 export default LoginPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 100vh;
+`;
+
+const Logo = styled.img`
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+  display: block;
+`;
