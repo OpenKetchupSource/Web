74d392a1e0440bbf6b05edb157a7351ffac4a56f
jungsunbeen <tjsqls041103@gmail.com>
2025-05-23 14:17:35 +0900

    feat: postSetting 함수의 매개변수 수정 및 API 호출 시 데이터 전송 추가

--- src/services/apis/chatting/chat.ts
+++ src/services/apis/chatting/chat.ts
@@ -2,9 +2,23 @@ import { CreateAxiosInstanceWithToken } from '../axiosInstanceWithToken';
 
 const axiosInstanceWithToken = CreateAxiosInstanceWithToken();
 
-export async function postSetting(id: string, pw: string) {
+export async function postSetting(memberId: string, characterId: string) {
   try {
-    const response = await axiosInstanceWithToken.post('/api/setting/initialize');
+    const response = await axiosInstanceWithToken.post('/api/setting/initialize', {
+      memberId,
+      characterId,
+    });
+    return response.data;
+  } catch (error) {
+    if (error) throw error;
+  }
+}
+
+export async function postCharacter(character: string) {
+  try {
+    const response = await axiosInstanceWithToken.post('/api/chat/start', {
+      character,
+    });
     return response.data;
   } catch (error) {
     if (error) throw error;
