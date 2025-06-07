ae97954c74344d4097cc87f4ff7a88265d8fbcb3
jungsunbeen <tjsqls041103@gmail.com>
2025-06-07 18:52:36 +0900

    Fix formatting in formatDate utility for consistency

--- src/components/common/FormatDate.ts
+++ src/components/common/FormatDate.ts
@@ -1,4 +1,4 @@
-export   const formatDate = (rawDate: string) => {
-    const date = new Date(rawDate);
-    return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, "0")}.${String(date.getDate()).padStart(2, "0")}.`;
-  };
+export const formatDate = (rawDate: string) => {
+  const date = new Date(rawDate);
+  return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, "0")}.${String(date.getDate()).padStart(2, "0")}.`;
+};
